9f4d3ed8272f3247e75f583fb16865b574f2549e
Vlad Huliaiev <gulyaev4420@gmail.com>
2020-09-14 17:54:53 +0300

    Updates

---
+++ precache-manifest.9d8866e8a846767eefa9efc3c2c819af.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a713b68f6a506a3131658d95a7b400",
+    "url": "/ReactJs-Start/index.html"
+  },
+  {
+    "revision": "196c3f3e2fce0c26f809",
+    "url": "/ReactJs-Start/static/css/main.cdea0ec9.chunk.css"
+  },
+  {
+    "revision": "03abace051d3d1f42cc1",
+    "url": "/ReactJs-Start/static/js/2.16ad72d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJs-Start/static/js/2.16ad72d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "196c3f3e2fce0c26f809",
+    "url": "/ReactJs-Start/static/js/main.b90eef9d.chunk.js"
+  },
+  {
+    "revision": "49c6a97f5ea20920db22",
+    "url": "/ReactJs-Start/static/js/runtime-main.9bf29d88.js"
+  },
+  {
+    "revision": "18867fcd477d88985ae98d33173185b6",
+    "url": "/ReactJs-Start/static/media/anakin_img.18867fcd.jpg"
+  },
+  {
+    "revision": "5b072750bef0d49b0587d3a1f990c5fd",
+    "url": "/ReactJs-Start/static/media/background.5b072750.jpg"
+  },
+  {
+    "revision": "3352fdeb8e3413067df241dbfdcd4031",
+    "url": "/ReactJs-Start/static/media/leia_img.3352fdeb.jpg"
+  },
+  {
+    "revision": "55da1bf0166a5eac98ee67e20a1ac573",
+    "url": "/ReactJs-Start/static/media/luke_img.55da1bf0.jpg"
+  },
+  {
+    "revision": "40b50c9e452e56b79e8bff1241aa3d54",
+    "url": "/ReactJs-Start/static/media/palpatine.40b50c9e.jpg"
+  },
+  {
+    "revision": "22de823b055e4f6b460f6a86c66042e8",
+    "url": "/ReactJs-Start/static/media/rey_avatar.22de823b.jpg"
+  },
+  {
+    "revision": "50847147924e88d4d429dc887b29341a",
+    "url": "/ReactJs-Start/static/media/rey_img.50847147.jpg"
+  },
+  {
+    "revision": "718524087d6534674e0793834348b97b",
+    "url": "/ReactJs-Start/static/media/rey_img_2.71852408.jpg"
+  },
+  {
+    "revision": "28584aa096f1429d5c4d19af7957e630",
+    "url": "/ReactJs-Start/static/media/rey_img_3.28584aa0.jpg"
+  },
+  {
+    "revision": "9febcbf7b40a922eaadf202a874fae14",
+    "url": "/ReactJs-Start/static/media/tick1.9febcbf7.png"
+  }
+]);
